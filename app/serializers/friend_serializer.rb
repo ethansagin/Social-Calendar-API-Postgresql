b4525bba860bb5d2ee3896fefeaa8f7d60508b91
ethansagin <ethansagin@gmail.com>
2019-11-12 15:29:25 -0500

    add api to git

---
+++ app/serializers/friend_serializer.rb
@@ -0,0 +1,5 @@
+class FriendSerializer < ActiveModel::Serializer
+    attributes :id, :name, :birthday, :visit_interval
+    has_many :meetups
+end
+  
---
+++ test/controllers/friends_controller_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class FriendsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @friend = friends(:one)
+  end
+
+  test "should get index" do
+    get friends_url, as: :json
+    assert_response :success
+  end
+
+  test "should create friend" do
+    assert_difference('Friend.count') do
+      post friends_url, params: { friend: { birthday: @friend.birthday, name: @friend.name, visit_interval: @friend.visit_interval } }, as: :json
+    end
+
+    assert_response 201
+  end
+
+  test "should show friend" do
+    get friend_url(@friend), as: :json
+    assert_response :success
+  end
+
+  test "should update friend" do
+    patch friend_url(@friend), params: { friend: { birthday: @friend.birthday, name: @friend.name, visit_interval: @friend.visit_interval } }, as: :json
+    assert_response 200
+  end
+
+  test "should destroy friend" do
+    assert_difference('Friend.count', -1) do
+      delete friend_url(@friend), as: :json
+    end
+
+    assert_response 204
+  end
+end
