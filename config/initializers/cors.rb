ae5d34f65e4dcf0782f0392544110c3e28e6ca1a
ethansagin <ethansagin@gmail.com>
2019-11-13 13:50:06 -0500

    reset cors urls to two origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    #['localhost:3000', 'https://social-calendar-client.herokuapp.com/']
+    origins ['localhost:3000', 'https://social-calendar-client.herokuapp.com/']
 
     resource '*',
       headers: :any,
